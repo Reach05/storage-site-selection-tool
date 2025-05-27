76fe9ca0dfc20ee9c68ba9cde9b64a0e744f1f45
Reach05 <reach@reachdmi.com>
2025-05-26 20:08:28 -0700

    Fix _app.jsx imports: remove diff markers and add SpeedInsights

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,12 @@
+// pages/_app.jsx
 import "../styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
