9ede39cb6d1250c98c389a43ee9fdbf821725226
Reach05 <reach@reachdmi.com>
2025-06-25 16:27:14 -0700

    chore: commit all changes

--- my-arcgis-app/next.config.js
+++ my-arcgis-app/next.config.js
@@ -1,7 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // …any rewrites, image-domains, allowedDevOrigins etc…
+  // …your other settings…
 };
 
-module.exports = nextConfig;
+export default nextConfig;
